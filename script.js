9d73799e63277e914f87fd33c38a1ed40d8cb0ee
qmya <qandeelgul90@gmail.com>
2020-12-14 08:56:22 -0500

    adding default values in functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+  console.log(bookings);
+};
+createBooking('LH123');
+createBooking('LH123', 1);
+createBooking('LH123', 1, 800);
+createBooking('LH123', undefined, 800);
