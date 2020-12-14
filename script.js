dac839529b75265d60d91c1312bf55c5bab492ad
qmya <qandeelgul90@gmail.com>
2020-12-14 09:17:52 -0500

    passing argument working: values vs. references

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ createBooking('LH123');
 createBooking('LH123', 1);
 createBooking('LH123', 1, 800);
 createBooking('LH123', undefined, 800);
+
+const flight = 'LH234';
+const qandeel = {
+  name: 'Qandeel Mya',
+  passport: 27346348734463278,
+};
+
+const checkIn = function (flightNum, passenger) {
+  //lets the flight number is changed
+  flightNum = 'EL456';
+  console.log(flightNum);
+  console.log(passenger);
+  passenger.name = 'Ms ' + passenger.name;
+
+  if (passenger.passport === 27346348734463278) {
+    console.log('CheckIn please');
+  } else {
+    console.log('Wrong passport');
+  }
+};
+
+checkIn(flight, qandeel);
+console.log(flight, qandeel);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+newPassport(qandeel); //It will give a new qandeel object who passport number will be different
+checkIn(flight, qandeel); // so that passport number will than go into the checkin function and will show that the passport is not equal to the original one
